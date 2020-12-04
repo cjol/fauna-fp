a6eda6c0f47604f14393fe702bdcce1a6b212225
Christopher Little <clittle@gsma.com>
2020-12-04 14:28:50 +0000

    Add curried select and merge functions

--- src/object.test.ts
+++ src/object.test.ts
@@ -1,43 +1,69 @@
-import { query as q } from "faunadb";
-import { expectTypeOf } from "expect-type";
-import { merge, select, selectDefault, toArray } from "./object";
-import { Arg, Query } from "./types";
+import { query as q } from 'faunadb';
+import { expectTypeOf } from 'expect-type';
+import { merge, select, selectDefault, toArray } from './object';
+import { Arg, Query } from './types';
+import { pipe } from 'fp-ts/lib/function';
+import { or } from './logic';
 
-describe("object", () => {
-  test("select", () => {
+describe('object', () => {
+  test('select', () => {
     const data = { foo: { bar: { zim: true }, baz: null } };
-    const intermediate = select(data, "foo", "bar");
-    const result = selectDefault(intermediate, null, "zim");
-
-    const input: Arg<{ name: string }> = { name: "hello" };
-    const r = select(input, "name");
-
+    const intermediate = select(data, 'foo', 'bar');
+    const intermediate2 = pipe(data, select('foo', 'bar'));
+    expect(intermediate).toEqual(intermediate2);
+    const result = selectDefault(null, 'zim')(intermediate);
     expectTypeOf(result).toEqualTypeOf<Query<boolean | null>>();
     expect(result).toEqual(
-      q.Select(["zim"], q.Select(["foo", "bar"], data), null as any)
+      q.Select(['zim'], q.Select(['foo', 'bar'], data), null as any)
     );
   });
 
-  test("merge", () => {
-    const data = merge(
-      { foo: true },
-      { bar: false },
-      (key, aVal, bVal) => aVal
-    );
-    expectTypeOf(data).toEqualTypeOf<Query<{ foo: boolean; bar: boolean }>>();
-    expect(data).toEqual(
+  test('merge', () => {
+    const one = { foo: true };
+    const two = { bar: false };
+    type Merged = { foo: boolean; bar: boolean };
+
+    const data1 = merge(one, two);
+    const data2 = merge(one)(two);
+    expectTypeOf(data1).toEqualTypeOf<Query<Merged>>();
+    expectTypeOf(data2).toEqualTypeOf<Query<Merged>>();
+    expect(data2).toEqual(data1);
+    expect(data1).toEqual(q.Merge(one, two));
+
+    const withResolver1 = merge((key, aVal, bVal) => aVal, one, two);
+    const withResolver2 = merge((key, aVal, bVal) => aVal, one)(two);
+    const withResolver3 = merge((key, aVal, bVal) => aVal)(one)(two);
+    const withResolver4 = merge((key, aVal, bVal) => aVal)(one, two);
+
+    expectTypeOf(withResolver1).toEqualTypeOf<Query<Merged>>();
+    expectTypeOf(withResolver2).toEqualTypeOf<Query<Merged>>();
+    expectTypeOf(withResolver3).toEqualTypeOf<Query<Merged>>();
+    expectTypeOf(withResolver4).toEqualTypeOf<Query<Merged>>();
+    expect(withResolver2).toEqual(withResolver1);
+    expect(withResolver3).toEqual(withResolver1);
+    expect(withResolver4).toEqual(withResolver1);
+    expect(withResolver1).toEqual(
       q.Merge(
         { foo: true },
         { bar: false },
-        q.Lambda(["key", "aVal", "bVal"], q.Var("aVal"))
+        q.Lambda(['key', 'aVal', 'bVal'], q.Var('aVal'))
       )
     );
+
+    // check that everything works well with a typed resolver
+    const mergeOr = merge((x, a: Arg<boolean>, b: Arg<boolean>) => or(a, b));
+    mergeOr({ foo: true }, { bar: false });
+    mergeOr({ foo: true })({ bar: false });
+    //@ts-expect-error
+    mergeOr({ foo: true }, { bar: 'false' });
+    //@ts-expect-error
+    mergeOr({ foo: true })({ bar: 'false' });
   });
 
-  test("toArray", () => {
-    const arr = toArray({ foo: true, bar: "hello" });
+  test('toArray', () => {
+    const arr = toArray({ foo: true, bar: 'hello' });
     expectTypeOf(arr).toEqualTypeOf<
-      Query<Array<["foo", boolean] | ["bar", string]>>
+      Query<Array<['foo', boolean] | ['bar', string]>>
     >();
   });
 });
--- src/object.ts
+++ src/object.ts
@@ -1,26 +1,83 @@
 import { O, A } from 'ts-toolbelt';
+import { curry } from 'ramda';
 import { Arg, Query, Callback, QueryResult } from './types';
 import { q } from './types.internal';
 
-export const select = <T extends object, P extends Array<A.Key>>(
+export function select<P extends Array<A.Key>>(
+  ...path: P
+): <T extends O.P.Record<P, any>>(
+  item: Arg<T>
+) => Query<QueryResult<O.Path<T, P>>>;
+export function select<T extends object, P extends Array<A.Key>>(
   item: Arg<T>,
   ...path: P
-): Query<QueryResult<O.Path<T, P>>> => q.Select(path, item);
+): Query<QueryResult<O.Path<T, P>>>;
+export function select(item?: any, ...path: (string | number)[]) {
+  if (typeof item === 'string' || typeof item === 'number') {
+    const fullPath = [item].concat(path);
+    return (item: object) => q.Select(fullPath, item);
+  }
+  return q.Select(path, item);
+}
 
-export const selectDefault = <T extends object, P extends Array<A.Key>, U>(
-  item: Arg<T>,
+export const selectDefault = <U, P extends Array<A.Key>>(
   fallback: Arg<U>,
   ...path: P
-): Query<QueryResult<O.Path<T, P>> | QueryResult<U>> =>
-  q.Select(path, item, fallback);
+) => <T extends O.P.Record<P, any>>(item: Arg<T>) =>
+  q.Select(path, item, fallback) as Query<QueryResult<U | O.Path<T, P>>>;
 
-// TODO: it's possible to do some very dynamic things here typewise; consider how to carefully allow
-// Indeed the types here are not great, especially with the resolver
-export const merge = <A extends object, B extends object>(
+type NotAFunction = { [k: string]: unknown } & (
+  | { bind?: never }
+  | { call?: never }
+);
+
+export function merge<A extends NotAFunction, B extends object>(
   a: Arg<A>,
-  b: Arg<B>,
-  resolver?: Callback<[keyof (A | B), A[keyof A], B[keyof B]], A[keyof A]>
-) => q.Merge(a, b, resolver) as Query<QueryResult<A> & QueryResult<B>>;
+  b: Arg<B>
+): Query<QueryResult<A> & QueryResult<B>>;
+export function merge<A extends NotAFunction>(
+  a: Arg<A>
+): <B extends object>(b: Arg<B>) => Query<QueryResult<A> & QueryResult<B>>;
+export function merge<A extends object, B extends object>(
+  resolver: Callback<[string, unknown, unknown], unknown>,
+  a: Arg<A>,
+  b: Arg<B>
+): Query<QueryResult<A> & QueryResult<B>>;
+export function merge<AVal, BVal, A extends Record<string, AVal>>(
+  resolver: Callback<[string, AVal, BVal], AVal | BVal>,
+  a: Arg<A>
+): <B extends Record<string, BVal>>(
+  b: Arg<B>
+) => Query<QueryResult<A> & QueryResult<B>>;
+export function merge<AVal, BVal>(
+  resolver: Callback<[string, AVal, BVal], AVal | BVal>
+): (<A extends Record<string, AVal>>(
+  a: Arg<A>
+) => <B extends Record<string, BVal>>(
+  b: Arg<B>
+) => Query<QueryResult<A> & QueryResult<B>>) &
+  (<A extends Record<string, AVal>, B extends Record<string, BVal>>(
+    a: Arg<A>,
+    b: Arg<B>
+  ) => Query<QueryResult<A> & QueryResult<B>>);
+export function merge(param1: any, param2?: any, param3?: any) {
+  if (typeof param1 === 'function') {
+    const resolver = param1;
+    if (param2) {
+      if (param3) {
+        return q.Merge(param2, param3, resolver);
+      }
+      return (param3: any) => q.Merge(param2, param3, resolver);
+    }
+    return curry((param2: any, param3: any) =>
+      q.Merge(param2, param3, resolver)
+    );
+  }
+  if (param2) {
+    return q.Merge(param1, param2);
+  }
+  return (param2: any) => q.Merge(param1, param2);
+}
 
 type Entries<T> = {
   [K in keyof T]: [K, T[K]];
